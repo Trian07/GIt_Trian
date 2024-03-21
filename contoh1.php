d8bdab08422a9b093454fd52d72e099c65e6c9c5
Trian07 <108216384+Trian07@users.noreply.github.com>
2024-03-21 18:19:53 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> perkenalan</h1>";
+        echo"Nama saya ramadhan
+             saya tinggal di cibitung
+             olahraga yang saya sukai adalah basket";
+    }
+}
